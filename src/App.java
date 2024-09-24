557881f8f1a05b4ccaeaa797ef99004e431c7af2
Noah Rubin <27nrubin@gannacademy.org>
2024-09-24 09:56:07 -0400

    ChatGPT

--- src/App.java
+++ src/App.java
@@ -4,4 +4,31 @@ public class App extends PApplet {
     public static void main(String[] args)  {
         PApplet.main("App");
     }
+
+    public void settings() {
+        size(400, 400);
+
+    }
+
+    public void setup() {
+        background(600);
+        
+        stroke(0, 250, 0);
+        strokeWeight(10);
+        fill(10,10,200);
+        rect(50, 50, 200, 100);
+
+        fill(255, 0, 0);
+        
+        
+        
+        
+        ellipse(400, 300, 100, 100);
+
+        stroke(0);
+        strokeWeight(5);
+        line(150, 100, 400, 250);
+
+    } 
 }
+
